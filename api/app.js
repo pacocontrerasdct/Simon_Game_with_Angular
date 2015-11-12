d3ed90bea2628971dd3de453a9454f01a969617d
Paco Contreras <pacocontrerasdct@gmail.com>
2015-11-12 09:20:27 +0000

    first try to deploy to heroku

--- api/app.js
+++ api/app.js
@@ -8,7 +8,10 @@ var app = express();
 
 // Connection to DB
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/simon-game-mean-app');
+
+// Connecting remotly using mongoLab or connecting locally to my DB
+var mongoUri =  process.env.MONGOLAB_URI || 'mongodb://localhost:27017/simon-game-mean-app';
+moongoose.connect(mongoUri);
 
 var routes = require('./config/routes');
 
@@ -19,5 +22,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(routes);
 
-app.listen(3000);
-console.log("Simon App listening on port 3000");
+app.listen(process.env.PORT || 3000 )
+console.log("Simon App is listening by local port 3000 or remotly!");
